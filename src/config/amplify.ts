080260e1a655c0c03e1dc1cf58bfab9f83cc9e5a
Aldo Zhang <aldozhang1@gmail.com>
2025-03-04 01:06:49 -0600

    fix amp

--- src/config/amplify.ts
+++ src/config/amplify.ts
@@ -1,20 +1,17 @@
 import { Amplify } from 'aws-amplify';
 
-Amplify.configure({
-    Auth: {
-      Cognito: {
-        userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID as string,
-        userPoolClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID as string,
-        loginWith: {
-          username: true
-        }
-      }
-    },
-    API: {
-      GraphQL: {
-        endpoint: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT as string,
-        region: process.env.NEXT_PUBLIC_AWS_REGION as string,
-        defaultAuthMode: 'userPool'
-      }
-    }
-  }); 
+const awsConfig = {
+  Auth: {
+    region: process.env.NEXT_PUBLIC_AWS_REGION!,
+    userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID!,
+    userPoolWebClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID!,
+  },
+  aws_appsync_graphqlEndpoint: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT!,
+  aws_appsync_region: process.env.NEXT_PUBLIC_AWS_REGION!,
+  aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+};
+
+// @ts-expect-error Amplify is not typed
+Amplify.configure(awsConfig); 
+
+export default awsConfig;
